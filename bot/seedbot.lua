3a5186bdf8a6277a1e62d554a31ddfd156939574
ABOL1378 <ABOLFAZL.ANVARI24@gmail.com>
2016-12-09 10:53:18 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,9 +230,13 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+"aparat",
+"plugins",
+"rmsg",
+
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(275579349)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
